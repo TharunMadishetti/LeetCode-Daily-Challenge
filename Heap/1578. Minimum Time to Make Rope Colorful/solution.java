90c81af52b4e67b84d7ac59d535b287d397b4b14
Tharun Madishetti <91481732+TharunMadishetti@users.noreply.github.com>
2022-10-03 12:09:39 +0530

    Update solution.java

--- Heap/1578. Minimum Time to Make Rope Colorful/solution.java
+++ Heap/1578. Minimum Time to Make Rope Colorful/solution.java
@@ -1,29 +1,30 @@
+//At first I thought, I need to store the elements in a priorityqueue but you don't need to as you just need one max value of the same group
+
+
 class Solution {
     public int minCost(String c, int[] t) {
         if(t.length==1)
             return 0;
-      PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());  // to maintain the times of consecutive elements and get maximum time of consecutive balloons
-        pq.add(t[0]);
+        int pq=t[0];
         int ans=t[0];
       for(int i=1;i<t.length;i++)
       {
         // System.out.println(i+"s "+pq + " "+ans);
 
-          if(c.charAt(i-1)==c.charAt(i))    //if this color is same as previous then keep on adding them into the queue
+          if(c.charAt(i-1)==c.charAt(i))
           {
-              pq.add(t[i]);
+              pq=Math.max(pq,t[i]);
           }
           else
           {
-              ans-=pq.poll();   //now we need to keep leave only that particular balloon which takes maximum time to remove hence we'll remove the maximum time needed balloon out of this
-              pq.clear(); //now let's start from the start as present color is diff previous color balloons doesn't influence present color
-              pq.add(t[i]); //again start adding these balloons 
+              ans-=pq;
+              pq=t[i]; 
           }
-          ans+=t[i];    //we add time needed for all balloons if they are consecutive then we'll leave only maximum time needed balloon
+          ans+=t[i];
             // System.out.println(i+" "+pq + " "+ans);
 
       }
-        ans-=pq.poll();   //queue still could not be empty there could be atleast last balloon or consecutive which are till the last index
+        ans-=pq;
         return ans;
     }
 }
