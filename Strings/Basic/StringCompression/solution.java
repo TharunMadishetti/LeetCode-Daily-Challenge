ffadfeb9730cea90b1fe388c96f85ad66a7101ac
Tharun Madishetti <91481732+TharunMadishetti@users.noreply.github.com>
2023-03-02 21:13:18 +0530

    Update solution.java

--- Strings/Basic/StringCompression/solution.java
+++ Strings/Basic/StringCompression/solution.java
@@ -1,6 +1,7 @@
 class Solution {
     public int compress(char[] chars) {
-        int i=0,j=0;
+        int i=0,j=0;        //i pointer for counting occurences
+                            //j pointer for holding the index to be filled 
         while(i<chars.length){
             char ch=chars[i];
             int c=0;
@@ -10,15 +11,16 @@ class Solution {
             }
             if(j<chars.length)
             chars[j++]=ch;
-            if(c/10==0){
-                if(c%10>1){
-                if(j<chars.length)
+            if(c/10==0){        //if the frequency is one digit
+                if(c%10>1){             //ignore if the frequency is only one
                 chars[j++]=(char)(c%10+'0');
                 }
-                continue;
+                continue;   
             }
-            String count=""+c;
-            int k=0;
+            //if the frequency is more than one digit 
+            String count=""+c;      
+            
+            int k=0;        //appending digit by digit
             while(k<count.length()){
                 if(j<chars.length)
                 chars[j++]=count.charAt(k++);
